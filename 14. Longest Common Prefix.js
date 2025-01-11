8588e516e31d4c660c09a894811ed2265d4481bc
BanerjeeProdipta <prodiptaj@live.com>
2025-01-10 20:25:25 -0500

    14. Longest Common Prefix

--- 14. Longest Common Prefix.js
+++ 14. Longest Common Prefix.js
@@ -1,14 +1,33 @@
-var longestCommonPrefix = function (strs) {
-  let matchedPrefix = '';
+// var longestCommonPrefix = function (strs) {
+//   let matchedPrefix = '';
 
-  for (let i = 0; i < strs[0].length; i++) {
-    if (strs.every((str) => str[i] === strs[0][i])) {
-      matchedPrefix += strs[0][i];
-    } else {
-      break;
-    }
+//   for (let i = 0; i < strs[0].length; i++) {
+//     if (strs.every((str) => str[i] === strs[0][i])) {
+//       matchedPrefix += strs[0][i];
+//     } else {
+//       break;
+//     }
+//   }
+//   return matchedPrefix;
+// };
+
+var longestCommonPrefix = function (strs) {
+  let str = ' ' + strs.join(' ');
+
+  let matchedPrefix = '';
+  if (strs.length === 1) return strs[0];
+
+  for (let i = 1; i < strs[0].length + 1; i++) {
+    let substr = strs[0].substring(0, i);
+
+    let res = [...str.matchAll(' ' + substr)].length;
+
+    if (res !== strs.length) {
+      return (matchedPrefix = substr.substring(0, substr.length - 1));
+    } else matchedPrefix = substr;
   }
+
   return matchedPrefix;
 };
 
-console.log(longestCommonPrefix([''])); // 'fl'
+console.log(longestCommonPrefix(['aa', 'aa']));
