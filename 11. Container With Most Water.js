d755d3a38b4ba2e807d1fe526ae3d6c67c1eaf64
Prodipta Banerjee <prodiptaj@live.com>
2025-08-16 15:15:53 -0400

    11

--- 11. Container With Most Water.js
+++ 11. Container With Most Water.js
@@ -4,20 +4,24 @@
  */
 
 var maxArea = function (height) {
+  let leftPointer = 0;
+  let rightPointer = height.length - 1;
   let max = 0;
-  for (let i = 0; i < height.length; i++) {
-    for (let j = i + 1; j < height.length; j++) {
-      const minHeightBetween = Math.min(height[i], height[j]);
-      const currArea = minHeightBetween * (j - i);
-      console.log({ i, elm: height[i], minHeightBetween, currArea });
 
-      if (max < currArea) max = currArea;
-    }
+  while (leftPointer !== rightPointer) {
+    let min = Math.min(height[leftPointer], height[rightPointer]);
+    let width = rightPointer - leftPointer;
+    const currArea = min * width;
+
+    if (currArea > max) max = currArea;
+    if (min === height[leftPointer]) {
+      leftPointer += 1;
+    } else rightPointer -= 1;
   }
   return max;
 };
 
-const test1 = [1, 8, 6, 2, 5, 4, 8, 3, 7];
+// const test1 = [1, 1];
 const test2 = [4, 3, 2, 1, 4];
 const res = maxArea(test2);
 
